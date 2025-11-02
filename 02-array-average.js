86bf84fc4dc249906d932d423336befba5910f4e
Belotti17 <belottiwenze@gmail.com>
2025-11-02 01:28:07 +0100

    feat resolve first exercice

--- 02-array-average.js
+++ 02-array-average.js
@@ -3,13 +3,36 @@
 // - Calcule la moyenne des notes
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
 
+const notes = [12, 8, 15, 9, 10];
 function averageNote(notes) {
-	
+  // Vérifie que notes est bien un tableau et qu'il n'est pas vide
+
+  if (!Array.isArray(notes) || notes.length === 0) {
+    return "Aucune note fournie";
+  }
+
+  //Initialisatin d'une variable pour la somme
+  let somme = 0;
+
+  //Parcourir le tableau avec une boucle for
+  for (let i = 0; i < notes.length; i++) {
+    somme += notes[i];
+  }
+
+  //Calculer la moyenne
+  const moyenne = somme / notes.length;
+
+  //Vérifie si la moyenne est suffisante pour réussir
+  if (moyenne >= 10) {
+    return "Réussi";
+  } else {
+    return "Échoué";
+  }
 }
 
+// affiche "Réussi" ou "Échoué" selon les notes
+console.log(averageNote(notes));
+
 module.exports = {
-	averageNote,
+  averageNote,
 };
-
-
-
