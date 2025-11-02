f84b8a1f47d9406230a5370de20f53727b049c0b
Belotti17 <belottiwenze@gmail.com>
2025-11-02 02:48:42 +0100

    fix the solution to the last problem to finish the exercise

--- 02-array-average.js
+++ 02-array-average.js
@@ -4,25 +4,25 @@
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
 
 const notes = [12, 8, 15, 9, 10];
+
 function averageNote(notes) {
   // Vérifie que notes est bien un tableau et qu'il n'est pas vide
-
   if (!Array.isArray(notes) || notes.length === 0) {
-    return "Échoué";
+    return "Échoué"; // Correction pour passer le test
   }
 
-  //Initialisatin d'une variable pour la somme
+  // Initialisation d'une variable pour la somme
   let somme = 0;
 
-  //Parcourir le tableau avec une boucle for
+  // Parcourir le tableau avec une boucle for
   for (let i = 0; i < notes.length; i++) {
     somme += notes[i];
   }
 
-  //Calculer la moyenne
+  // Calculer la moyenne
   const moyenne = somme / notes.length;
 
-  //Vérifie si la moyenne est suffisante pour réussir
+  // Vérifie si la moyenne est suffisante pour réussir
   if (moyenne >= 10) {
     return "Réussi";
   } else {
@@ -30,7 +30,7 @@ function averageNote(notes) {
   }
 }
 
-// affiche "Réussi" ou "Échoué" selon les notes
+// Affiche "Réussi" ou "Échoué" selon les notes
 console.log(averageNote(notes));
 
 module.exports = {
